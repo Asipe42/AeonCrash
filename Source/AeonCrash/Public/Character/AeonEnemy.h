e3006e3e265724b44694a17fb246dd9b2bf036e2
modin <eom3658@gmail.com>
2025-03-06 22:10:20 +0900

    AeonEnmey 생성

---
+++ Source/AeonCrash/Public/Character/AeonEnemy.h
@@ -0,0 +1,17 @@
+// Copyright Modin
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AeonCharacterBase.h"
+#include "AeonEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AEONCRASH_API AAeonEnemy : public AAeonCharacterBase
+{
+	GENERATED_BODY()
+	
+};
