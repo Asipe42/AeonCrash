7e319312f5a5ae5e711f194eec1e2486e051b656
modin <eom3658@gmail.com>
2025-03-06 22:09:27 +0900

    AeonCharacter 생성

---
+++ Source/AeonCrash/Public/Character/AeonCharacter.h
@@ -0,0 +1,17 @@
+// Copyright Modin
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AeonCharacterBase.h"
+#include "AeonCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AEONCRASH_API AAeonCharacter : public AAeonCharacterBase
+{
+	GENERATED_BODY()
+	
+};
